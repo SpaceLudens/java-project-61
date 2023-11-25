81c501eb5cd50ab2cc8b1ac2da6e0a679c95e51f
Rustam <SpaceLudens1@gmail.com>
2023-11-25 17:35:54 +0300

    add Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,13 +1,10 @@
-
 package hexlet.code.games;
 
-import hexlet.code.Engine;
 import static hexlet.code.Engine.*;
-
 public class Even {
     public static void game() {
         for (int i = 0; i < roundsCount; i++) {
-            int randomNumber = Engine.getRandomNumber(1, 100);
+            int randomNumber = getRandomNumber(1, 100);
             question[i] = Integer.toString(randomNumber);
             result[i] = randomNumber % 2 == 0 ? "yes" : "no";
         }
