5c5753012894a2361010a5c6cb317d74972214ab
Rustam <SpaceLudens1@gmail.com>
2023-11-25 17:34:01 +0300

    add Even.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -17,7 +17,8 @@ public class Engine {
                 "Answer 'yes' if the number is even, otherwise answer 'no'.",
                 "What is the result of the expression?",
                 "Find the greatest common divisor of given numbers.",
-                "What number is missing in the progression?"};
+                "What number is missing in the progression?",
+                "Answer 'yes' if given number is prime. Otherwise answer 'no'." };
         return gamesRules[gameNumber];
     }
 
--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -1,2 +1,25 @@
 package hexlet.code.games;
-public class Prime {}
+
+import static hexlet.code.Engine.*;
+
+public class Prime {
+    public static void game() {
+
+        for (int i = 0; i < roundsCount; i++) {
+
+            int randomNumber = getRandomNumber(1,10);
+            question[i] = String.valueOf(randomNumber);
+
+            for (int j = 2; j <= randomNumber ; j++) {
+                if (randomNumber % j == 0 && j != randomNumber) {
+                    result[i] = "no";
+                    break;
+                } else if (randomNumber % j == 0 && j == randomNumber) {
+                    result[i] = "yes";
+                    break;
+                }
+            }
+        }
+        gameLauncher(4, question , result);
+    }
+}
