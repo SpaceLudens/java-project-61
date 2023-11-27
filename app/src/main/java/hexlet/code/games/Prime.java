dd900790fa65d17a8af9bf93feedb3fd0c69291c
Rustam <SpaceLudens1@gmail.com>
2023-11-27 22:05:34 +0300

    error correction

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -1,28 +1,28 @@
 package hexlet.code.games;
 
+import static hexlet.code.Engine.question;
 import static hexlet.code.Engine.roundsCount;
+import static hexlet.code.Engine.result;
 import static hexlet.code.Engine.gameLauncher;
 import static hexlet.code.RandomNumbers.getRandomNumber;
 
 public class Prime {
     public static void game() {
 
-        String [] questions = new String[roundsCount];
-        String [] answers = new String[roundsCount];
-
         for (int i = 0; i < roundsCount; i++) {
+
             int randomNumber = getRandomNumber(1, 100);
-            questions[i] = String.valueOf(randomNumber);
+            question[i] = String.valueOf(randomNumber);
 
             for (int j = 2; j <= randomNumber / 2; j++) {
                 if (randomNumber % j == 0) {
-                    answers[i] = "no";
+                    result[i] = "no";
                     break;
                 } else {
-                    answers[i] = "yes";
+                    result[i] = "yes";
                 }
             }
         }
-        gameLauncher(4, questions, answers);
+        gameLauncher(4, question, result);
     }
 }
