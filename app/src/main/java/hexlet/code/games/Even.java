2afbc2d30d54b376ec051ce0ddbbd173afbee980
Rustam <SpaceLudens1@gmail.com>
2023-12-03 21:49:49 +0300

    Bug fix

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,5 +1,8 @@
 package hexlet.code.games;
 
+import static hexlet.code.Engine.getQuestion;
+import static hexlet.code.Engine.getResult;
+import static hexlet.code.Engine.gameLauncher;
 import static hexlet.code.Engine.getRoundsCount;
 import static hexlet.code.Engine.NUMBER_TO_GENERATE_A_RANDOM_NUMBER_1;
 import static hexlet.code.Engine.NUMBER_TO_GENERATE_A_RANDOM_NUMBER_10;
@@ -8,13 +11,12 @@ import static hexlet.code.RandomNumbers.getRandomNumber;
 
 public class Even {
     private static final String GAME_RULE = "Answer 'yes' if the number is even, otherwise answer 'no'.";
-    private static String[] questions = new String[getRoundsCount()];
-    private static  String[] correctAnswers = new String[getRoundsCount()];
     public static void game() {
         for (int i = 0; i < getRoundsCount(); i++) {
             int randomNumber = getRandomNumber(NUMBER_TO_GENERATE_A_RANDOM_NUMBER_1, NUMBER_TO_GENERATE_A_RANDOM_NUMBER_10);
-            questions[i] = Integer.toString(randomNumber);
-            correctAnswers[i] = randomNumber % 2 == 0 ? "yes" : "no";
+            getQuestion()[i] = Integer.toString(randomNumber);
+            getResult()[i] = randomNumber % 2 == 0 ? "yes" : "no";
         }
+        gameLauncher(GAME_RULE, getQuestion(), getResult());
     }
 }
